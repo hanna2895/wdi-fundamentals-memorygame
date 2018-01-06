c7bafc933d27a1096616b010c4116efcb0f29a64
Hannah Werman <hannahwerman@Hannahs-MacBook-Air.local>
2018-01-06 14:50:49 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen.");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flpped king.");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	}
+}
